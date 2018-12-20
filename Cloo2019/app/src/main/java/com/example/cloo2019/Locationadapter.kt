f9a545c240bf186bfa09e7e887786c9ac7fc0b51
kartik-v1 <45596681+kartik-v1@users.noreply.github.com>
2018-12-20 11:02:32 +0530

    Code Cleanup
    Unwanted commented code removed from Locationadaptor.kt

--- Cloo2019/app/src/main/java/com/example/cloo2019/Locationadapter.kt
+++ Cloo2019/app/src/main/java/com/example/cloo2019/Locationadapter.kt
@@ -1,12 +1,8 @@
 /* Created by Kartik Venkataraman, 14 Nov 2018 */
 
 package com.example.cloo2019
-
-import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
-import android.location.Location
-import android.net.Uri
 import android.support.v4.content.ContextCompat.startActivity
 import android.view.LayoutInflater
 import android.view.View
@@ -26,19 +22,10 @@ class Locationadapter(val mCtx: Context, val layoutResId: Int, val locationList:
 
         val layoutInflater: LayoutInflater = LayoutInflater.from(mCtx)
         val view: View = layoutInflater.inflate(layoutResId, null)
-
         val textViewLooName = view.findViewById<TextView>(R.id.textView_looName)
-        //        val textViewLooSelect = view.findViewById<TextView>(R.id.textView_selectLoo)
-
-        // KARTIK _28_NOV_2018: has to be modified to retrive, latlng, address.
-        // need to get the current latlng to compute the distance!
-        // how do I get the current latlng here? should I just pass them as argument for Locationadaptor class?
 
         val currentLocation = CurrentLocation.getLastLocation()
         val loo = locationList[position]
-
-        // KARTIK _28_NOV_2018: Change the text to show the name and address of the loo. Would also need to have provision to show the distance
-
         val distanceLat = loo.latval - currentLocation!!.latitude
         val distanceLng = loo.lngVal - currentLocation!!.longitude
 
@@ -69,68 +56,15 @@ class Locationadapter(val mCtx: Context, val layoutResId: Int, val locationList:
 
         textViewLooName.text = loo.address + "\nRating [" + ratings +"]      " + distanceStr
 
-
-/*        textViewLooName.text = loo.address + "\n" + distanceLat.toString() + "," + distanceLng.toString() + " ltlg away" +
-                "\nRating = " + loo.userRating
-*/
-/*        textViewLooName.text = "Lat: " + loo.latval.toString() + "Lng: " + loo.lngVal.toString() + "Alt: " +
-                loo.altVal.toString() + "Rating: " + loo.userRating.toString() + "Comments: " + loo.userComments
-*/
-//        textViewLooSelect.setOnClickListener {
-            textViewLooName.setOnClickListener {
-
+        textViewLooName.setOnClickListener {
             val i = Intent(mCtx, MapsActivity_Directions::class.java)
-
             i.putExtra("LAT", loo.latval)
             i.putExtra("LNG", loo.lngVal)
             i.putExtra("RATING", loo.userRating)
             i.putExtra("TOILET_ADDRESS", loo.address)
             i.putExtra("TOILET_ID", loo.id )
             startActivity(mCtx,i,null)
-
-//            showUpdateDialog(loo)
         }
-
-
         return view
     }
-
-    fun showUpdateDialog(loo: cUserInput) {
-        val builder = AlertDialog.Builder(mCtx)
-
-        val inflater = LayoutInflater.from(mCtx)
-
-        val view = inflater.inflate(R.layout.layout_select_loo, null)
-
-        val textViewSelectedLoo = view.findViewById<TextView>(R.id.textview_Location)
-
-
-
-
-        textViewSelectedLoo.text = "Lat: " + loo.latval.toString() + " Lng: " + loo.lngVal.toString() +
-                " Alt: " + loo.altVal.toString() + " Rat: " + loo.userRating.toString() +
-                " Comments: "+ loo.userComments
-
-
-
-
-        builder.setView(view)
-
-
-
-        builder.setPositiveButton("Ok") { dialog, which ->
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            return@setPositiveButton
-        }
-
-/*        builder.setNegativeButton("No") { dialog, which ->
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        }
-*/
-        val alert = builder.create()
-        alert.show()
-
-
-    }
-
 }
--- ReadMe.txt
+++ ReadMe.txt
@@ -10,5 +10,10 @@ Edited by Ashwin
     is now shown as stars in the landing page instead of a number.
     -*-
 
-    
+0.02 Dec 17, 2018 Kartik Venkataraman
+    Issues to be addressed
+    a) The app crashes the first time it runs
+    b) CurrentLocation Object is not updated when the location is refreshed
+       from other activities.  Might have to include this code there and/or
+       make those activites use this object value.
 
